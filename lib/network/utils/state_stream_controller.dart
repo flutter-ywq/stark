edd9a9e20b2f1ceca531534de11005cebc61b1be
燕文强 <yanwenqiang1991@foxmail.com>
2020-07-27 23:47:58 +0800

    T bloc(BuildContext context)

--- lib/network/utils/state_stream_controller.dart
+++ lib/network/utils/state_stream_controller.dart
@@ -7,17 +7,17 @@ import 'package:stark/network/state_bo.dart';
 ///
 /// @date 2020/7/16
 class StateStreamController<T extends StateBo> {
-  StreamController<T> controller;
+  StreamController<StateBo<T>> controller;
 
   StateStreamController() {
-    controller = StreamController<T>();
+    controller = StreamController<StateBo<T>>();
   }
 
-  Stream<T> get stream => controller.stream;
+  Stream<StateBo<T>> get stream => controller.stream;
 
-  StreamSink<T> get _sink => controller.sink;
+  StreamSink<StateBo<T>> get _sink => controller.sink;
 
-  void add(T item) {
+  void add(StateBo<T> item) {
     if (!controller.isClosed || controller.isPaused) {
       _sink.add(item);
     }
